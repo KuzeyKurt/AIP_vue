db9a4d267dc611de8d297d1fb94d2446f520274c
KuzeyKurt <72691339+KuzeyKurt@users.noreply.github.com>
2021-12-14 02:46:02 +0300

    store created

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify' // орор
+import vuetify from './plugins/vuetify' // пїЅпїЅпїЅпїЅ
 import Router from 'vue-router'
 import router from './router/index'
+import store from './store'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   vuetify,
     render: h => h(App),
-  router: router
+  router: router,
+  store
 }).$mount('#app')
